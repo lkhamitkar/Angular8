a564242a1ecc01652c7c485567a59eb9f8885b0a
lkhamitkar <lkhamitkar49@gmail.com>
2019-09-19 21:32:55 +0530

    data binding and simple service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishDetailComponent } from './dishdetail/dishdetail.component';
-
+import { DishService } from './services/dish.service'
 
 @NgModule({
   declarations: [
@@ -33,7 +33,7 @@ import { DishDetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [ DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
