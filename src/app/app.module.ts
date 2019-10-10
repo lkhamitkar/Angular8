92a589649bb30a03eac0fb9592f092f1e1eb1699
lkhamitkar <lkhamitkar49@gmail.com>
2019-10-10 22:00:02 +0530

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,9 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component'
+import { ContactComponent } from './contact/contact.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { LoginComponent } from './login/login.component'
 
 
 @NgModule({
@@ -32,7 +34,8 @@ import { ContactComponent } from './contact/contact.component'
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
     
   ],
   imports: [
@@ -44,7 +47,11 @@ import { ContactComponent } from './contact/contact.component'
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   providers: [ DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
