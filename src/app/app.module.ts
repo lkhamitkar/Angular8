b8c4be279028d1256566f4d82c190ef28781ca7e
lkhamitkar <lkhamitkar49@gmail.com>
2019-10-10 22:06:30 +0530

    template form1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,11 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 
 @NgModule({
@@ -48,7 +52,11 @@ import { LoginComponent } from './login/login.component'
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   entryComponents: [
     LoginComponent
